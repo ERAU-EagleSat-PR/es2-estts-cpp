eb53c76bbc329a7ebc0825e4256fb33131a40455
Hayden Roszell <hroszell@gmail.com>
2021-12-11 00:57:58 -0700

    Test interface for serial controller [ESTTS-70]

--- examples/estts-demo1/src/main.cpp
+++ examples/estts-demo1/src/main.cpp
@@ -3,8 +3,23 @@
 //
 
 #include <iostream>
-#include "system_api.h"
+#include "ti_serial_handler.h"
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+
+    const char * port = "/dev/tty.usbmodem2101";
+    int baud = 115200;
+
+    auto serial = new ti_serial_handler(port, baud);
+    unsigned char msg[] = { 'H', 'e', 'l', 'l', 'o', '\r', '\0' };
+
+    unsigned char* bruh = serial->read_serial_uc();
+    std::cout << bruh << std::endl;
+
+    std::string bruh2 = serial->read_serial_s();
+    std::cout << bruh2 << std::endl;
+    // serial->write_serial_uc(msg);
+
+    delete serial;
+
 }
