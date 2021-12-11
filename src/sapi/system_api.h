d9e5c2f7371aeec30af2823d630d8bd78e49b551
Hayden Roszell <hroszell@gmail.com>
2021-12-11 00:58:46 -0700

    Scaffolding for system API [ESTTS-54]

--- src/sapi/system_api.h
+++ src/sapi/system_api.h
@@ -5,30 +5,48 @@
 #ifndef ES2_ESTTS_CPP_SYSTEM_API_H
 #define ES2_ESTTS_CPP_SYSTEM_API_H
 
+#include <iostream>
 #include "constants.h"
+#include "InfoField.h"
+using namespace std;
 
-typedef struct AX25FrameHeader {
-    const unsigned char * destAddr; // Unsigned char -> [0,255]
-    const unsigned char * ssid0; // These values are constant, see constants.h
-    const unsigned char * srcAddr;
-    const unsigned char * ssid1;
-    const unsigned char * control;
-    const unsigned char * pid;
-} AX25FrameHeader;
+namespace sapi {
+    typedef struct SAPICommandStructure {
+        unsigned char address;
+        unsigned char command;
+        unsigned char method;
+        unsigned char * body;
+    } SAPICommandStructure;
+}
 
-typedef struct AX25Frame {
-    const unsigned char * flag; // Unsigned char -> [0,255]
-    AX25FrameHeader header;
-    const unsigned char crc16[16];
-    const unsigned char * information_field;
-
-} AX25Frame;
-
-class system_api {
+class System_API {
 private:
+    const unsigned char flag; // Unsigned char -> [0,255]
 
+    /* AX.25 frame header */
+    // These values are constant, see constants.h
+    const unsigned char * destAddr;
+    const unsigned char ssid0;
+    const unsigned char * srcAddr;
+    const unsigned char ssid1;
+    const unsigned char control;
+    const unsigned char pid;
+
+    /* AX.25 frame body */
+    unsigned char * crc16;
+    unsigned char * information_field;
+    sapi::SAPICommandStructure * command;
+    std::string buildFrameHeader();
 public:
+    explicit System_API() : flag(estts::AX25_FLAG), destAddr(estts::AX25_DESTINATION_ADDRESS), ssid0(estts::AX25_SSID0),
+                            srcAddr(estts::AX25_SOURCE_ADDRESS), ssid1(estts::AX25_SSID1), control(estts::AX25_CONTROL),
+                            pid(estts::AX25_PID), crc16(nullptr), information_field(nullptr), command(nullptr) {}
 
+    explicit System_API(sapi::SAPICommandStructure * command) : System_API() {
+        this->information_field = (unsigned char *)0x55555; // TODO - populate information field inside constructor
+        this->command = command;
+    };
+    static estts::Status buildAX25Frame();
 };
 
 
